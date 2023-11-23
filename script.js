4d90e9a754d827af2aad31159bb6f2166f1b4933
kofta999 <mostafaxxx555@gmail.com>
2023-11-23 19:19:39 +0200

    Add basic arithmatic functions

---
+++ script.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+function divide(num1, num2) {
+  return num1 / num2;
+}
