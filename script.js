9b89a379368c42b15951f4a0ece4dd3975af7016
kofta999 <mostafaxxx555@gmail.com>
2023-11-23 19:56:53 +0200

    Add HTML main body and number selection

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+
+let num1, operand, num2;
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    let num = parseInt(e.target.textContent);
+    console.log(num);
+    if (!num2 && num1 && operand) num2 = num;
+    else num1 = num;
+  });
+});
+
+function operate(num1, operand, num2) {
+  switch (operand) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}
+
 /**
  *
  * @param {number} num1
